3292d8661b3bbe459695cc49df450ad2a13a93dc
Daren Gallery <darengallery@gmail.com>
2020-05-28 19:17:28 +0100

    code

--- PHP/ifa/sendmail.php
+++ PHP/ifa/sendmail.php
@@ -7,10 +7,10 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 $formcontent=" From: $name \n IFA Membership number: $ifaMembership \n Practice Name: $practiceName \n Email: $email \n Contact Phone: $phone \n Enquiry: $message";
-$recipient = "darengallery@gmail.com";
+$recipient = "matthew@metiscompliance.co.uk";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-header('Location: http://www.gallerytest.online/thank_you.html');
+header('Location: http://www.metiscompliance.co.uk/thank_you.html');
 exit;
 ?>
