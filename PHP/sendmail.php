45181d0e83024f45c36849186ea35152965b362f
Daren Gallery <darengallery@gmail.com>
2020-05-28 19:16:08 +0100

    added code

--- PHP/sendmail.php
+++ PHP/sendmail.php
@@ -5,10 +5,10 @@ $contactPhone = $_POST['contactPhone'];
 $message = $_POST['message'];
 
 $formcontent=" From: $name \n Email: $email \n Contact Phone: $contactPhone \n Message: $message";
-$recipient = "darengallery@gmail.com";
+$recipient = "matthew@metiscompliance.co.uk";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-header('Location: http://www.gallerytest.online/thank_you.html');
+header('Location: http://www.metiscompliance.co.uk/thank_you.html');
 exit;
 ?>
