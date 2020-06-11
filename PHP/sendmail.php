02757349ecfe00534615ec1faa0f45a4c8607897
Daren Gallery <darengallery@gmail.com>
2020-06-11 22:33:16 +0100

    added code

--- PHP/sendmail.php
+++ PHP/sendmail.php
@@ -6,7 +6,7 @@ $contactPhone = $_POST['contactPhone'];
 $message = $_POST['message'];
 
 $formcontent=" From: $name \n Email: $email \n Practice Name: $practiceName \n Contact Phone: $contactPhone \n Message: $message";
-$recipient = "matthew@metiscompliance.co.uk";
+$recipient = "metismatt@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
